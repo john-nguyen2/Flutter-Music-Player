2036c2fe8b9f8a5b5c91dc8420cd31bde45dc0ef
John Nguyen <jbn180001@utdallas.edu>
2021-08-09 13:16:24 -0700

    summer

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/johnq/Documents/flutter-2"
+export "FLUTTER_APPLICATION_PATH=/Users/johnq/Documents/GitHub/Flutter-Music-Player"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/johnq/Documents/GitHub/Flutter-Music-Player/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/johnq/Documents/GitHub/Flutter-Music-Player/.dart_tool/package_config.json"
